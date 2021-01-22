915744031d9a5f739050135562e5260155ed18a3
Deepzirox <deepzirox@localhost.localdomain>
2021-01-21 22:43:40 -0500

    fix

--- 0x00-ls/hls.c
+++ 0x00-ls/hls.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 	OnlyFolders(argc - 1, (argv + 1), finfo);
 	if (opt->err != 0)
 	{
-		printf("hls: invalid option '%c'\n", opt->err);
+		printf("hls: invalid option -- %c\n", opt->err);
 		errno = 2;
 	}
 	switch (finfo->err)
