75ed8dee3f936019859fdf2dd963f1eb8cfd8363
Camilo <toboncamilo33@gmail.com>
2021-03-25 23:16:27 -0500

    add

--- 0x05-libasm/100-puti.asm
+++ 0x05-libasm/100-puti.asm
@@ -1,75 +1,74 @@
 BITS 64
-	global asm_puti		; export 'asm_puti' function
-
-	extern asm_putc		; import 'asm_putc' function
-
+	global asm_puti
+	extern asm_putc
 	section .text
 
 asm_puti:
-
 	push rbp
 	mov rbp, rsp
-	sub rsp, 20
+	push r15
+ 	push r14
+	push r13
 
-asm_puti_rax:
+	xor r15, r15
+	xor r14, r14	; r14 is our char counter
+	movsx r15, edi	; r15 holds our integer n
+	xor rax, rax
 
-	mov qword [rsp - 12], 0 ; save return value on the stack
-
-asm_puti_n:
-
-	mov [rsp - 8], edi	; copy @n onto the stack
-
-asm_puti_sign:
-
-	mov dword [rsp - 4], 1	; copy 1 onto the stack
-
-	cmp edi, 0		; compare 0 with @n
-
-	jge asm_puti_char	; jump to "char" if @n is non-negative
-
-	neg dword [rsp - 4]	; flip the sign
-
-asm_puti_char:
-
-	mov dword [rsp], 0x30
-	xor edx, edx
-	mov eax, edi
-	mov edi, 10
-	idiv edi
-	mov eax, edx
-	imul dword [rsp - 4]
-	add [rsp], eax
-
-asm_puti_loop:
-
-	cmp dword [rsp - 8], 0
-	jge asm_puti_loop_div
-	mov edi, 0x2D
+	cmp r15, 0
+	jge test
+	imul r15, -1
+	mov r13, 1
+	mov rdi, 45
 	call asm_putc
-	inc qword [rsp - 12]
 
-asm_puti_loop_div:
-	xor edx, edx
-	mov eax, [rsp - 8]
-	mov edi, 10
-	idiv edi
-	cmp eax, 0
-	je asm_puti_loop_putc
-	imul dword [rsp - 4]
-	mov edi, eax
-	call asm_puti
-	add [rsp - 12], rax
-
-asm_puti_loop_putc:
-
-	mov edi, [rsp]
+test:
+	test r15, r15
+	jnz wh
+	mov rdi, 48
 	call asm_putc
-	inc qword [rsp - 12]
+	mov r13, 1
 
-asm_puti_end:
+wh:
+	; RAX starts as dividend becomes quotient
+	; RDX receives remainder
+	xor rdx, rdx
+	test r15, r15
+	jz done_wh
+	mov rax, r15
+	mov rbx, 10
+	div rbx
+	; print remainder in RDX
+	mov r15, rax
+	after_div:
+	push rdx
+	after_push:
+	inc r14
+	jmp wh
+done_wh:
 
-	mov rax, [rsp - 12]
-	add rsp, 20
+	mov r15, r14;	move counter
+print:
+	test r15, r15
+	jz done_print
+	pop rdi
+	after_pop:
+	add rdi, 48
+	call asm_putc
+	dec r15
+	jmp print
+
+done_print:
+
+	cmp r13, 1
+	jne end
+	inc r14
+
+end:
+	mov rax, r14
+	pop r13
+	pop r14
+	pop r15
 	mov rsp, rbp
 	pop rbp
 	ret
