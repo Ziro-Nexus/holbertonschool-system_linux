900d5728c851d34b7d47daa810c53d4be899a691
Camilo <toboncamilo33@gmail.com>
2021-03-25 23:04:37 -0500

    add

--- 0x05-libasm/7-puts.asm
+++ 0x05-libasm/7-puts.asm
@@ -1,24 +1,21 @@
 BITS 64
-	global asm_puts		; export 'asm_puts' function
-	extern asm_strlen	; import 'asm_strlen' function
+	global asm_puts
+	extern asm_strlen
 	section .text
-
-
 asm_puts:
 	push rbp
 	mov rbp, rsp
 	sub rsp, 12
 	mov [rsp], rdi
-	call asm_strlen	
+	call asm_strlen
 	mov [rsp - 8], eax
 	mov rax, 1
 	mov rdi, 1
 	mov edx, [rsp - 8]
 	mov rsi, [rsp]
-	syscall	
-
+	syscall
 asm_puts_end:
-	add rsp, 12	
+	add rsp, 12
 	mov rsp, rbp
-	pop rbp	
-	ret	
+	pop rbp
+	ret
